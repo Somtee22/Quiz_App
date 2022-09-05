4b207409ddc5784b4eae20d9bbf892f23936ae94
scholastica ngene <nnemms@gmail.com>
2022-09-05 04:05:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Hyper Text Preprocessor
+// Hyper Text Multiple Language
+// Hyper Text Markup Language
+// Hyper Tool Multiple language
+
+Choose the correct HTML element for the largest heading:
+
+What is the correct HTML element for inserting a line break?
+
+Choose the correct HTML element to define important text
+
+
+
+
+
+
+
+// What Does CSS Stand for?
+// Common Style Sheet
+// Cascading Style Sheet
+// Computer Style Sheet
+// Colourful Style Sheet
