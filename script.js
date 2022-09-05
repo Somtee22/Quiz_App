77e40b66c856a8d79387c43f986a5cce852aa88c
scholastica ngene <nnemms@gmail.com>
2022-09-05 04:25:38 -0700

    m

--- script.js
+++ script.js
@@ -3,9 +3,25 @@
 // Hyper Text Markup Language
 // Hyper Tool Multiple language
 
-Choose the correct HTML element for the largest heading:
+// Choose the correct HTML element for the largest heading:
+// <h1>
+// <h6>
+// <heading>
+// <head>
 
-What is the correct HTML element for inserting a line break?
+// What is the correct HTML element for inserting a line break?
+// <break>
+// <lb>
+// <br>
+// <b>
+
+
+
+
+
+
+
+
 
 Choose the correct HTML element to define important text
 
