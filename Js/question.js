fb40efb9c745054f7a891f47229e35ed3dce85a5
scholastica ngene <nnemms@gmail.com>
2022-09-06 13:24:43 +0100

    added Js folder

---
+++ Js/question.js
@@ -0,0 +1,71 @@
+let questions = [
+ 
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  {
+  numb: 6,
+  question: "Your Question is Here",
+  answer: "Correct answer of the question is here",
+  options: [
+    "Option 1",
+    "option 2",
+    "option 3",
+    "option 4"
+  ]
+},
+];
+
+// Choose the correct HTML element for the largest heading:
+// <h1>
+// <h6>
+// <heading>
+// <head>
+
+// What is the correct HTML element for inserting a line break?
+// <break>
+// <lb>
+// <br>
+// <b>
+
+
+
+  
+
+
+
+
+
+// What Does CSS Stand for?
+// Common Style Sheet
+// Cascading Style Sheet
+// Computer Style Sheet
+// Colourful Style Sheet
+//selecting all required elements
+
+
+
+
+
+
